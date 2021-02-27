8543489c39ba8354df7fc464b81288537876410e
HanulM <hanul.m26@gmail.com>
2021-02-27 12:40:57 -0500

    created architecture and added game object fields

---
+++ wichacks-2021/wichacks-2021/GameObject.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace wichacksSpring
+{
+    abstract class GameObject
+    {
+        #region Fields
+        private Texture2D asset;
+        private Rectangle position;
+        #endregion
+
+        #region Properties
+        public Rectangle Position { get { return this.position; } }
+        #endregion
+
+        #region Constructor
+        public GameObject(Texture2D asset, Rectangle pos)
+        {
+            this.asset = asset;
+            this.position = pos;
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Tells the object to draw itself
+        /// </summary>
+        public virtual void Draw()
+        {
+
+        }
+        #endregion
+    }
+}
