0ff516efeaf6a210e58c47d7c17b59be3dd6a771
HanulM <hanul.m26@gmail.com>
2021-02-27 12:52:27 -0500

    finished player class fields and constructor

--- wichacks-2021/wichacks-2021/Player.cs
+++ wichacks-2021/wichacks-2021/Player.cs
@@ -1,10 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace wichacksSpring
 {
     class Player : GameObject
     {
+        #region Fields
+        private Texture2D asset;
+        private Rectangle position;
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Constructor
+        public Player(Texture2D asset, Rectangle pos)
+            : base(asset, pos)
+        {
+            this.asset = asset;
+            this.position = pos;
+        }
+        #endregion
+
+        #region Methods
+        public virtual void Draw()
+        {
+            
+        }
+        #endregion
     }
 }
