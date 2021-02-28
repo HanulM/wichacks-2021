8d17ae11eb4a1915dfab36663f90498eb96bbda8
HanulM <hanul.m26@gmail.com>
2021-02-27 21:47:18 -0500

    loaded in assets

--- wichacks-2021/wichacks-2021/Game1.cs
+++ wichacks-2021/wichacks-2021/Game1.cs
@@ -20,6 +20,22 @@ namespace wichacksSpring
         private SpriteBatch _spriteBatch;
         private GameState currentState;
 
+        // Asset Fields
+        private Texture2D titleScreen;
+        private Texture2D enterButton;
+        private Texture2D tourGuide;
+
+        // Object Fields
+        private Exhibit firstExhibit;
+        private Exhibit secondExhibit;
+        private Exhibit thirdExhibit;
+
+        // Keyboard and Mouse Input
+        private KeyboardState currentKbState;
+        private KeyboardState prevKbState;
+        private MouseState currentMouseState;
+        private MouseState prevMouseState;
+
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -30,7 +46,10 @@ namespace wichacksSpring
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-
+            _graphics.PreferredBackBufferWidth = 1600;
+            _graphics.PreferredBackBufferHeight = 1000;
+            _graphics.ApplyChanges();
+            currentState = GameState.TitleScreen;
             base.Initialize();
         }
 
@@ -39,6 +58,7 @@ namespace wichacksSpring
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            titleScreen = this.Content.Load<Texture2D>("titlescreen-background");
         }
 
         protected override void Update(GameTime gameTime)
@@ -79,7 +99,32 @@ namespace wichacksSpring
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            switch (currentState)
+            {
+                case GameState.TitleScreen:
+                    _spriteBatch.Draw(titleScreen, new Rectangle(0, 0, 1600, 1000), Color.White);
+                    break;
 
+                case GameState.GameIntro:
+                    break;
+
+                case GameState.FirstScene:
+                    break;
+
+                case GameState.SecondScene:
+                    break;
+
+                case GameState.ThirdScene:
+                    break;
+
+                case GameState.EndScreen:
+                    break;
+
+                case GameState.Credits:
+                    break;
+            }
+            _spriteBatch.End();
             base.Draw(gameTime);
         }
     }
